cdba7fa2b1ec1bda7f04a8db59ed88f9a79c488f
Edgard Carrillo <ea_carrillo@hotmail.com>
2024-01-23 08:09:07 -0500

    update23Ene02

--- app.js
+++ app.js
@@ -10,12 +10,11 @@ const path = require('path')
 
 dbconnect(app)
 
-//app.use(cors())
-app.use(cors({credentials: true}))
-// app.use(cors({
-//     origin: 'http://localhost:3000/', // Reemplaza con el origen correcto de tu frontend
-//     credentials: true, // Permite el intercambio de cookies a través de las solicitudes
-//   }));
+// app.use(cors({credentials: true}))
+app.use(cors({
+    origin: process.env.URL_PAGE, // Reemplaza con el origen correcto de tu frontend
+    credentials: true, // Permite el intercambio de cookies a través de las solicitudes
+  }));
 app.use(express.json())
 
 app.use('/api/v1/data',formcontroller)
