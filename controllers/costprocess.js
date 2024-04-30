c88d771a0fd064daf7fe41e3ac9d013a73ee43bb
Edgard Carrillo <ea_carrillo@hotmail.com>
2024-04-30 11:45:38 -0500

    Update30Abr01

--- controllers/costprocess.js
+++ controllers/costprocess.js
@@ -111,8 +111,52 @@ const getalldataactualplanta = async (req, res) => {
 
     console.log("ejecutando get all data planta");
     // const data = await actualPlantamodel.find({})
-    const data = await actualPlantamodel.find({ Mes: { $ne: 0 } })
-    res.status(200).json(data)
+    // const data = await actualPlantamodel.find({ Mes: { $ne: 0 } })
+    // res.status(200).json(data)
+
+
+    try {
+        //const batchSize = 1000; // Tamaño del lote
+
+        // Establecemos el encabezado Content-Type como JSON
+        res.setHeader('Content-Type', 'application/json');
+
+        res.write('['); // Comenzamos el arreglo JSON
+
+        let isFirst = true; // Para determinar si es el primer elemento del arreglo
+
+        //let skip = 0;
+
+        const cursor = actualPlantamodel.find({ Mes: { $ne: 0 } })
+            .lean()
+            .cursor(); // Obtener un stream de documentos
+
+        cursor.on('data', (item) => {
+            // Envía los datos al cliente línea por línea en formato JSON
+            if (!isFirst) {
+                res.write(',');
+            } else {
+                isFirst = false;
+            }
+            res.write(JSON.stringify(item));
+            console.log("ejecutando");
+        });
+
+        cursor.on('end', () => {
+            res.write(']'); // Finalizamos el arreglo JSON
+            res.end(); // Finalizamos la respuesta
+            console.log("Finalizado");
+        });
+
+        cursor.on('error', (error) => {
+            console.error('Error al leer los datos:', error);
+            res.status(500).json({ error: 'Ocurrió un error al leer los datos.' });
+        });
+
+    } catch (error) {
+        console.error('Error al leer los datos:', error);
+        res.status(500).json({ error: 'Ocurrió un error al leer los datos.' });
+    }
 
 }
 
